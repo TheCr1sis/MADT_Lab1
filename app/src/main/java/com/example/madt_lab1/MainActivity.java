f0f6ef7f6a62051c3a8122b467582c1a541bb336
TheCr1sis <bulgarilya@gmail.com>
2023-09-09 14:19:22 +0300

    Change text button added

--- app/src/main/java/com/example/madt_lab1/MainActivity.java
+++ app/src/main/java/com/example/madt_lab1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.madt_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View v){
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Completely new text!");
     }
 }
